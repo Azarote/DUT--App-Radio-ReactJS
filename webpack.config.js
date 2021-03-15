b11a1e809c6f6ba58a4112cf05d35c2fe25aa5e5
Azarote <matteo_munoz@hotmail.fr>
2021-03-15 15:14:14 +0100

    init webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ 
+ module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    indexcss: './src/index.css',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: 'RadioListener',
+    }),
+  ],
+   output: {
+    filename: 'bundle.js',
+    filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   
+   module:{
+    rules: 
+    [            {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+    },
+        {
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options:{
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }]
+   }
+
+ };
