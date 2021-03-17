0ab46bd58e17365be6debb8eba26ab996325ada6
mattduf13 <matduf13@gmail.com>
2021-03-18 00:05:13 +0100

    additional style

--- src/components/Lecteur.jsx
+++ src/components/Lecteur.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 class Lecteur extends React.Component {
-    render(){
+    render() {
         var image = null;
 
-        if(this.props.currentRadio!=null)
-            image = require('../ressources/images/'+this.props.currentRadio.img).default;
+        if (this.props.currentRadio != null)
+            image = require('../ressources/images/' + this.props.currentRadio.img).default;
 
-        return(
+        return (
             <div id="container-lecteur">
                 {
                     this.props.currentRadio == null
-                    ?
-                    <div id="lecteur-empty">Aucune radio sélectionnée.</div>:
-                    [<img key={0} src={image} alt="image de la radio"/>,<h4 key={1}>{this.props.currentRadio.name}</h4>]
+                        ?
+                        <div id="lecteur-empty">Aucune radio sélectionnée.</div> :
+                        <div id="lecteur-img-h4"><img src={image} alt="image de la radio"/><h4>{this.props.currentRadio.name}</h4></div>
                 }
             </div>
         );
